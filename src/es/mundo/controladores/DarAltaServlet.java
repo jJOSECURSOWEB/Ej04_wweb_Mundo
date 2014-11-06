665462ad45f67dbc708941455c699a4d4d673405
1 <1@04-PC.04-PC>
2014-11-06 12:24:07 +0100

    inicial Casos de Uso: Dar Alta, ConsultarUno, Consultartodos

---
+++ src/es/mundo/controladores/DarAltaServlet.java
@@ -0,0 +1,48 @@
+package es.mundo.controladores;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import es.mundo.modelo.Negocio;
+
+/**
+ * Servlet implementation class DarAltaServlet
+ */
+@WebServlet("/DarAlta")
+public class DarAltaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DarAltaServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//1 recuperar datos URL
+		String nombre=request.getParameter("nombre");
+		//2 adaptarlossi es necesarioal tipode datosdelmodel(convertir de string a entero X ejem)
+		int habitantes=Integer.parseInt(request.getParameter("habitantes"));
+		//3 pasarle los datos al negocio
+		Negocio negocio=new Negocio();
+		int id=negocio.darAlta(nombre,habitantes);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
